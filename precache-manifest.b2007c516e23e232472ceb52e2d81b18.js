e95ad9617e0ff70c6e414b95a68ba903cd0de64c
VuPhamCongMinh <lonelymoon206@gmail.com>
2020-08-16 19:04:56 +0700

    Updates

---
+++ precache-manifest.b2007c516e23e232472ceb52e2d81b18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7884040ff2203515575af5a2d5942309",
+    "url": "/reactjs-fetchWeather/index.html"
+  },
+  {
+    "revision": "1a011a766d4e40715b94",
+    "url": "/reactjs-fetchWeather/static/css/2.81df9be0.chunk.css"
+  },
+  {
+    "revision": "179e4d8e239b01d26424",
+    "url": "/reactjs-fetchWeather/static/css/main.0faac3ba.chunk.css"
+  },
+  {
+    "revision": "1a011a766d4e40715b94",
+    "url": "/reactjs-fetchWeather/static/js/2.feb43e1d.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/reactjs-fetchWeather/static/js/2.feb43e1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "179e4d8e239b01d26424",
+    "url": "/reactjs-fetchWeather/static/js/main.7e8696ba.chunk.js"
+  },
+  {
+    "revision": "e13355ae43a4a597da4d",
+    "url": "/reactjs-fetchWeather/static/js/runtime-main.5c4b5944.js"
+  }
+]);
